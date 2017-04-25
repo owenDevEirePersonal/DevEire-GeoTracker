1ed97839d9222a4cf624f038d62544338bf213f8
owenDevEirePersonal <owen@deveire.com>
2017-04-25 10:01:59 +0100

    <Start of Service Branch> Start of the code branch that attempts to move the data and processing away from the mapsactivity and into its own service so that it will run and update in the background.

---
+++ app/src/main/java/com/deveire/dev/deveiregeofindergolf/TrackingService.java
@@ -0,0 +1,19 @@
+package com.deveire.dev.deveiregeofindergolf;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TrackingService extends Service
+{
+    public TrackingService()
+    {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
